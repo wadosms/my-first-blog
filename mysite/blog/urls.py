f68e94431317e1803ae38d4803af67aa937d8896
vadim <wadosms@gmail.com>
2018-01-02 23:24:06 +0100

    Fixed bug

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url
+from django.urls import path
 from .  import views
 
 urlpatterns = [
-	url('', views.post_list, name='post_list'),
-	url('^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail')
+	path('', views.post_list, name='post_list'),
+	path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
